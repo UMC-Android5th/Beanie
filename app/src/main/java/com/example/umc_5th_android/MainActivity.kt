b59c4f5d8a09aebb065a2b63eb39fc7d34e3d043
leeeyubin <youbeene3409@naver.com>
2023-09-27 00:49:58 +0900

    bottom_menu.xml

--- app/src/main/java/com/example/umc_5th_android/MainActivity.kt
+++ app/src/main/java/com/example/umc_5th_android/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.umc_5th_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.umc_5th_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.containers, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.bottomNavigationview.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.HomeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.containers, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.LookFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.containers, LookFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.SearchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.containers, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.LockerFragment ->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.containers, LockerFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
