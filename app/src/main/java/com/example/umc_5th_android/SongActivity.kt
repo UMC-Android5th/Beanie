ffa4674ce355de2c6f817d0c015873838eebcf8c
leeeyubin <youbeene3409@naver.com>
2023-10-05 07:38:34 +0900

    [feat] : SongAcitivty에 기능 추가

--- app/src/main/java/com/example/umc_5th_android/SongActivity.kt
+++ app/src/main/java/com/example/umc_5th_android/SongActivity.kt
@@ -2,12 +2,18 @@ package com.example.umc_5th_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.umc_5th_android.databinding.ActivityMainBinding
 import com.example.umc_5th_android.databinding.ActivitySongBinding
 
 class SongActivity : AppCompatActivity() {
     lateinit var binding : ActivitySongBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_song)
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.songDown.setOnClickListener {
+            finish()
+        }
     }
 }
