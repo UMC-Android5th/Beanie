99bd3fa988c88cad76ae3c15853267b909c00772
leeeyubin <youbeene3409@naver.com>
2023-10-31 03:00:23 +0900

    [DEL] : 주석 삭제

--- app/src/main/java/com/example/umc_5th_android/Fragment/LockerFragment.kt
+++ app/src/main/java/com/example/umc_5th_android/Fragment/LockerFragment.kt
@@ -13,7 +13,6 @@ class LockerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_locker, container, false)
     }
 
--- app/src/main/java/com/example/umc_5th_android/Fragment/LookFragment.kt
+++ app/src/main/java/com/example/umc_5th_android/Fragment/LookFragment.kt
@@ -14,7 +14,6 @@ class LookFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_look, container, false)
     }
 
