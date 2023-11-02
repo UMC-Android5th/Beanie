1b44fdadd91fb9417f431470c72c7f3df1135fdd
leeeyubin <youbeene3409@naver.com>
2023-11-02 20:13:15 +0900

    [FEAT] : 뮤직 플레이 기능

--- app/src/main/java/com/example/umc_5th_android/Song.kt
+++ app/src/main/java/com/example/umc_5th_android/Song.kt
@@ -2,8 +2,10 @@ package com.example.umc_5th_android
 
 data class Song(
     val title : String = "",
-    val singer: String = "",
-    val second : Int = 0,
+    val singer: String = "" ,
+    var second : Int = 0,
     var playTime : Int = 0,
-    var isPlaying : Boolean = false
+    var isPlaying : Boolean = false,
+    var music : String = "",
+    var isLike: Boolean = false
 )
